7916a8ce59a31db2d4b7f5af91a23159a0944fee
gwang307 <gwang307@gatech.edu>
2024-03-11 16:41:11 -0400

    First commit

---
+++ src/main/java/com/revature/georgeproject/GeorgeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.revature.georgeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeorgeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeorgeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/georgeproject/GeorgeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.georgeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeorgeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
